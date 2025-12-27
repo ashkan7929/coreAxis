e52e3a8d674d1625545bd2c18c8fc4fbf4be4c1a
ashkan7929 <ashkan.ansari.aa@gmail.com>
2025-12-27 09:51:59 +0330

    feat(auth): add standard CRUD actions and wallet/MLM permissions
    Add standard CRUD actions (Read, Manage, Create, Update, Delete) and enhance wallet/MLM permissions with additional fields. Also improve role permission assignments with null checks and additional fields.

--- src/Modules/AuthModule/Infrastructure/CoreAxis.Modules.AuthModule.Infrastructure/Migrations/20251214115413_AddWalletAndMLMPermissions.cs
+++ src/Modules/AuthModule/Infrastructure/CoreAxis.Modules.AuthModule.Infrastructure/Migrations/20251214115413_AddWalletAndMLMPermissions.cs
@@ -14,8 +14,39 @@ namespace CoreAxis.Modules.AuthModule.Infrastructure.Migrations
                 -- Insert 'MarkPaid' Action
                 IF NOT EXISTS (SELECT 1 FROM Actions WHERE Code = 'MarkPaid')
                 BEGIN
-                    INSERT INTO Actions (Id, Code, Name, Description, CreatedOn, CreatedBy, IsActive)
-                    VALUES (NEWID(), 'MarkPaid', 'Mark Paid', 'Mark commission as paid', GETDATE(), 'System', 1)
+                    INSERT INTO Actions (Id, Code, Name, Description, SortOrder, CreatedOn, CreatedBy, LastModifiedBy, LastModifiedOn, IsActive)
+                    VALUES (NEWID(), 'MarkPaid', 'Mark Paid', 'Mark commission as paid', 0, GETDATE(), 'System', 'System', GETDATE(), 1)
+                END
+
+                -- Ensure standard Actions exist
+                IF NOT EXISTS (SELECT 1 FROM Actions WHERE Code = 'Read')
+                BEGIN
+                    INSERT INTO Actions (Id, Code, Name, Description, SortOrder, CreatedOn, CreatedBy, LastModifiedBy, LastModifiedOn, IsActive)
+                    VALUES (NEWID(), 'Read', 'Read', 'Read permission', 1, GETDATE(), 'System', 'System', GETDATE(), 1)
+                END
+
+                IF NOT EXISTS (SELECT 1 FROM Actions WHERE Code = 'Manage')
+                BEGIN
+                    INSERT INTO Actions (Id, Code, Name, Description, SortOrder, CreatedOn, CreatedBy, LastModifiedBy, LastModifiedOn, IsActive)
+                    VALUES (NEWID(), 'Manage', 'Manage', 'Manage permission', 2, GETDATE(), 'System', 'System', GETDATE(), 1)
+                END
+
+                IF NOT EXISTS (SELECT 1 FROM Actions WHERE Code = 'Create')
+                BEGIN
+                    INSERT INTO Actions (Id, Code, Name, Description, SortOrder, CreatedOn, CreatedBy, LastModifiedBy, LastModifiedOn, IsActive)
+                    VALUES (NEWID(), 'Create', 'Create', 'Create permission', 3, GETDATE(), 'System', 'System', GETDATE(), 1)
+                END
+
+                IF NOT EXISTS (SELECT 1 FROM Actions WHERE Code = 'Update')
+                BEGIN
+                    INSERT INTO Actions (Id, Code, Name, Description, SortOrder, CreatedOn, CreatedBy, LastModifiedBy, LastModifiedOn, IsActive)
+                    VALUES (NEWID(), 'Update', 'Update', 'Update permission', 4, GETDATE(), 'System', 'System', GETDATE(), 1)
+                END
+
+                IF NOT EXISTS (SELECT 1 FROM Actions WHERE Code = 'Delete')
+                BEGIN
+                    INSERT INTO Actions (Id, Code, Name, Description, SortOrder, CreatedOn, CreatedBy, LastModifiedBy, LastModifiedOn, IsActive)
+                    VALUES (NEWID(), 'Delete', 'Delete', 'Delete permission', 5, GETDATE(), 'System', 'System', GETDATE(), 1)
                 END
 
                 -- Insert Pages
@@ -27,8 +58,8 @@ namespace CoreAxis.Modules.AuthModule.Infrastructure.Migrations
                 IF NOT EXISTS (SELECT 1 FROM Pages WHERE Code = 'WALLET')
                 BEGIN
                     SET @WalletPageId = NEWID()
-                    INSERT INTO Pages (Id, Code, Name, ModuleName, Description, CreatedOn, CreatedBy, IsActive)
-                    VALUES (@WalletPageId, 'WALLET', 'Wallet', 'WalletModule', 'Wallet management', GETDATE(), 'System', 1)
+                    INSERT INTO Pages (Id, Code, Name, ModuleName, Description, SortOrder, CreatedOn, CreatedBy, LastModifiedBy, LastModifiedOn, IsActive)
+                    VALUES (@WalletPageId, 'WALLET', 'Wallet', 'WalletModule', 'Wallet management', 0, GETDATE(), 'System', 'System', GETDATE(), 1)
                 END
                 ELSE
                 BEGIN
@@ -38,8 +69,8 @@ namespace CoreAxis.Modules.AuthModule.Infrastructure.Migrations
                 IF NOT EXISTS (SELECT 1 FROM Pages WHERE Code = 'Commissions')
                 BEGIN
                     SET @CommissionsPageId = NEWID()
-                    INSERT INTO Pages (Id, Code, Name, ModuleName, Description, CreatedOn, CreatedBy, IsActive)
-                    VALUES (@CommissionsPageId, 'Commissions', 'Commissions', 'MLMModule', 'Commission management', GETDATE(), 'System', 1)
+                    INSERT INTO Pages (Id, Code, Name, ModuleName, Description, SortOrder, CreatedOn, CreatedBy, LastModifiedBy, LastModifiedOn, IsActive)
+                    VALUES (@CommissionsPageId, 'Commissions', 'Commissions', 'MLMModule', 'Commission management', 0, GETDATE(), 'System', 'System', GETDATE(), 1)
                 END
                 ELSE
                 BEGIN
@@ -49,8 +80,8 @@ namespace CoreAxis.Modules.AuthModule.Infrastructure.Migrations
                 IF NOT EXISTS (SELECT 1 FROM Pages WHERE Code = 'CommissionRules')
                 BEGIN
                     SET @CommissionRulesPageId = NEWID()
-                    INSERT INTO Pages (Id, Code, Name, ModuleName, Description, CreatedOn, CreatedBy, IsActive)
-                    VALUES (@CommissionRulesPageId, 'CommissionRules', 'Commission Rules', 'MLMModule', 'Commission rules management', GETDATE(), 'System', 1)
+                    INSERT INTO Pages (Id, Code, Name, ModuleName, Description, SortOrder, CreatedOn, CreatedBy, LastModifiedBy, LastModifiedOn, IsActive)
+                    VALUES (@CommissionRulesPageId, 'CommissionRules', 'Commission Rules', 'MLMModule', 'Commission rules management', 0, GETDATE(), 'System', 'System', GETDATE(), 1)
                 END
                 ELSE
                 BEGIN
@@ -60,8 +91,8 @@ namespace CoreAxis.Modules.AuthModule.Infrastructure.Migrations
                 IF NOT EXISTS (SELECT 1 FROM Pages WHERE Code = 'UserReferrals')
                 BEGIN
                     SET @UserReferralsPageId = NEWID()
-                    INSERT INTO Pages (Id, Code, Name, ModuleName, Description, CreatedOn, CreatedBy, IsActive)
-                    VALUES (@UserReferralsPageId, 'UserReferrals', 'User Referrals', 'MLMModule', 'User referrals management', GETDATE(), 'System', 1)
+                    INSERT INTO Pages (Id, Code, Name, ModuleName, Description, SortOrder, CreatedOn, CreatedBy, LastModifiedBy, LastModifiedOn, IsActive)
+                    VALUES (@UserReferralsPageId, 'UserReferrals', 'User Referrals', 'MLMModule', 'User referrals management', 0, GETDATE(), 'System', 'System', GETDATE(), 1)
                 END
                 ELSE
                 BEGIN
@@ -87,43 +118,43 @@ namespace CoreAxis.Modules.AuthModule.Infrastructure.Migrations
                 -- WALLET:Read
                 IF NOT EXISTS (SELECT 1 FROM Permissions WHERE PageId = @WalletPageId AND ActionId = @ReadActionId)
                 BEGIN
-                    INSERT INTO Permissions (Id, PageId, ActionId, Name, Description, CreatedOn, CreatedBy, IsActive)
-                    VALUES (NEWID(), @WalletPageId, @ReadActionId, 'WALLET:Read', 'WALLET Read permission', GETDATE(), 'System', 1)
+                    INSERT INTO Permissions (Id, PageId, ActionId, Name, Description, CreatedOn, CreatedBy, LastModifiedBy, LastModifiedOn, IsActive)
+                    VALUES (NEWID(), @WalletPageId, @ReadActionId, 'WALLET:Read', 'WALLET Read permission', GETDATE(), 'System', 'System', GETDATE(), 1)
                 END
 
                 -- WALLET:Manage
                 IF NOT EXISTS (SELECT 1 FROM Permissions WHERE PageId = @WalletPageId AND ActionId = @ManageActionId)
                 BEGIN
-                    INSERT INTO Permissions (Id, PageId, ActionId, Name, Description, CreatedOn, CreatedBy, IsActive)
-                    VALUES (NEWID(), @WalletPageId, @ManageActionId, 'WALLET:Manage', 'WALLET Manage permission', GETDATE(), 'System', 1)
+                    INSERT INTO Permissions (Id, PageId, ActionId, Name, Description, CreatedOn, CreatedBy, LastModifiedBy, LastModifiedOn, IsActive)
+                    VALUES (NEWID(), @WalletPageId, @ManageActionId, 'WALLET:Manage', 'WALLET Manage permission', GETDATE(), 'System', 'System', GETDATE(), 1)
                 END
 
                 -- Commissions:Read
                 IF NOT EXISTS (SELECT 1 FROM Permissions WHERE PageId = @CommissionsPageId AND ActionId = @ReadActionId)
                 BEGIN
-                    INSERT INTO Permissions (Id, PageId, ActionId, Name, Description, CreatedOn, CreatedBy, IsActive)
-                    VALUES (NEWID(), @CommissionsPageId, @ReadActionId, 'Commissions:Read', 'Commissions Read permission', GETDATE(), 'System', 1)
+                    INSERT INTO Permissions (Id, PageId, ActionId, Name, Description, CreatedOn, CreatedBy, LastModifiedBy, LastModifiedOn, IsActive)
+                    VALUES (NEWID(), @CommissionsPageId, @ReadActionId, 'Commissions:Read', 'Commissions Read permission', GETDATE(), 'System', 'System', GETDATE(), 1)
                 END
 
                 -- Commissions:MarkPaid
                 IF NOT EXISTS (SELECT 1 FROM Permissions WHERE PageId = @CommissionsPageId AND ActionId = @MarkPaidActionId)
                 BEGIN
-                    INSERT INTO Permissions (Id, PageId, ActionId, Name, Description, CreatedOn, CreatedBy, IsActive)
-                    VALUES (NEWID(), @CommissionsPageId, @MarkPaidActionId, 'Commissions:MarkPaid', 'Commissions MarkPaid permission', GETDATE(), 'System', 1)
+                    INSERT INTO Permissions (Id, PageId, ActionId, Name, Description, CreatedOn, CreatedBy, LastModifiedBy, LastModifiedOn, IsActive)
+                    VALUES (NEWID(), @CommissionsPageId, @MarkPaidActionId, 'Commissions:MarkPaid', 'Commissions MarkPaid permission', GETDATE(), 'System', 'System', GETDATE(), 1)
                 END
 
                 -- CommissionRules:Create
                 IF NOT EXISTS (SELECT 1 FROM Permissions WHERE PageId = @CommissionRulesPageId AND ActionId = @CreateActionId)
                 BEGIN
-                    INSERT INTO Permissions (Id, PageId, ActionId, Name, Description, CreatedOn, CreatedBy, IsActive)
-                    VALUES (NEWID(), @CommissionRulesPageId, @CreateActionId, 'CommissionRules:Create', 'CommissionRules Create permission', GETDATE(), 'System', 1)
+                    INSERT INTO Permissions (Id, PageId, ActionId, Name, Description, CreatedOn, CreatedBy, LastModifiedBy, LastModifiedOn, IsActive)
+                    VALUES (NEWID(), @CommissionRulesPageId, @CreateActionId, 'CommissionRules:Create', 'CommissionRules Create permission', GETDATE(), 'System', 'System', GETDATE(), 1)
                 END
 
                 -- UserReferrals:Create
                 IF NOT EXISTS (SELECT 1 FROM Permissions WHERE PageId = @UserReferralsPageId AND ActionId = @CreateActionId)
                 BEGIN
-                    INSERT INTO Permissions (Id, PageId, ActionId, Name, Description, CreatedOn, CreatedBy, IsActive)
-                    VALUES (NEWID(), @UserReferralsPageId, @CreateActionId, 'UserReferrals:Create', 'UserReferrals Create permission', GETDATE(), 'System', 1)
+                    INSERT INTO Permissions (Id, PageId, ActionId, Name, Description, CreatedOn, CreatedBy, LastModifiedBy, LastModifiedOn, IsActive)
+                    VALUES (NEWID(), @UserReferralsPageId, @CreateActionId, 'UserReferrals:Create', 'UserReferrals Create permission', GETDATE(), 'System', 'System', GETDATE(), 1)
                 END
                 
                 -- Assign Permissions to Roles
@@ -134,18 +165,24 @@ namespace CoreAxis.Modules.AuthModule.Infrastructure.Migrations
                 SELECT @AdminRoleId = Id FROM Roles WHERE Name = 'Admin'
 
                 -- Assign all Read permissions to User role
-                INSERT INTO RolePermissions (RoleId, PermissionId, CreatedOn, CreatedBy, AssignedAt, AssignedBy)
-                SELECT @UserRoleId, p.Id, GETDATE(), 'System', GETDATE(), '00000000-0000-0000-0000-000000000000'
-                FROM Permissions p
-                JOIN Actions a ON p.ActionId = a.Id
-                WHERE a.Code = 'Read'
-                AND NOT EXISTS (SELECT 1 FROM RolePermissions rp WHERE rp.RoleId = @UserRoleId AND rp.PermissionId = p.Id)
+                IF @UserRoleId IS NOT NULL
+                BEGIN
+                    INSERT INTO RolePermissions (Id, RoleId, PermissionId, CreatedOn, CreatedBy, LastModifiedBy, LastModifiedOn, AssignedAt, AssignedBy, IsActive)
+                    SELECT NEWID(), @UserRoleId, p.Id, GETDATE(), 'System', 'System', GETDATE(), GETDATE(), '00000000-0000-0000-0000-000000000000', 1
+                    FROM Permissions p
+                    JOIN Actions a ON p.ActionId = a.Id
+                    WHERE a.Code = 'Read'
+                    AND NOT EXISTS (SELECT 1 FROM RolePermissions rp WHERE rp.RoleId = @UserRoleId AND rp.PermissionId = p.Id)
+                END
 
                 -- Assign ALL permissions to Admin role
-                INSERT INTO RolePermissions (RoleId, PermissionId, CreatedOn, CreatedBy, AssignedAt, AssignedBy)
-                SELECT @AdminRoleId, p.Id, GETDATE(), 'System', GETDATE(), '00000000-0000-0000-0000-000000000000'
-                FROM Permissions p
-                WHERE NOT EXISTS (SELECT 1 FROM RolePermissions rp WHERE rp.RoleId = @AdminRoleId AND rp.PermissionId = p.Id)
+                IF @AdminRoleId IS NOT NULL
+                BEGIN
+                    INSERT INTO RolePermissions (Id, RoleId, PermissionId, CreatedOn, CreatedBy, LastModifiedBy, LastModifiedOn, AssignedAt, AssignedBy, IsActive)
+                    SELECT NEWID(), @AdminRoleId, p.Id, GETDATE(), 'System', 'System', GETDATE(), GETDATE(), '00000000-0000-0000-0000-000000000000', 1
+                    FROM Permissions p
+                    WHERE NOT EXISTS (SELECT 1 FROM RolePermissions rp WHERE rp.RoleId = @AdminRoleId AND rp.PermissionId = p.Id)
+                END
             ");
         }
 
